30a99d0144567b5cb7aa349e2398482d60133bc3
igor hirsch <g1rsh.egor@gmail.com>
2024-07-31 20:48:25 +0200

    Home and Detail Page

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,48 +1,42 @@
-// Importiert die Modellklasse ErrorViewModel
+using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
-
-
-// Importiert die MVC-Bibliothek
 using Microsoft.AspNetCore.Mvc;
-
-// Importiert die System.Diagnostics Bibliothek
 using System.Diagnostics;
 
-// Definiert den Namespace für den Controller
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
-    // Definiert den HomeController, der von der Controller-Basis-Klasse erbt
     public class HomeController : Controller
     {
-        // Deklariert ein Feld für den Logger
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        // Konstruktor, der den Logger initialisiert
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
-        // Index-Aktion, um die Startseite anzuzeigen
         public IActionResult Index()
         {
-            // Gibt die Index-View zurück
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
-        // Aktion, um die Datenschutzseite anzuzeigen
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
+
         public IActionResult Privacy()
         {
-            // Gibt die Privacy-View zurück
             return View();
         }
 
-        // Aktion, um die Fehlerseite anzuzeigen
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            // Gibt die Error-View mit einem ErrorViewModel zurück, das die aktuelle Anforderungs-ID enthält
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
